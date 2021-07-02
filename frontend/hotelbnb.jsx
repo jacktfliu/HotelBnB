ba307eda6e00adca53e80d0f23464f0cd683bd11
Jack Liu <Jack.tf.liu@gmail.com>
2021-07-02 17:01:36 -0400

    test

--- frontend/hotelbnb.jsx
+++ frontend/hotelbnb.jsx
@@ -4,4 +4,4 @@ import ReactDOM from "react-dom";
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to HotelBnB</h1>, root);
-});
+})
