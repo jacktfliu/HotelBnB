b10296e19ce78057c102f398af5074fd78334648
Jack Liu <Jack.tf.liu@gmail.com>
2021-07-14 17:00:27 -0400

    placeholder search bar splash page

--- frontend/components/splash/background_body.jsx
+++ frontend/components/splash/background_body.jsx
@@ -5,8 +5,20 @@ import {Link} from 'react-router-dom'
 const BackgroundBody = () => {
     return(
         <div className='splash-container'>
-            
-            {/* <div className='background-word'>Not sure where to go? Perfect.</div> */}
+            <div className='filter-box'>
+                <div className='filter-container'>
+                    <button className='filter-buttons'>
+                    <div>Where are you going?</div></button>
+                    <span></span>
+                    <input type='date' className='filter-buttons'/>
+                    <span></span>
+                    <input type='date' className='filter-buttons'/>
+                    <span></span>
+                    <button className='filter-buttons'>Guest</button>
+                    <span></span>
+                    <Link to='/listings' className='filter-buttons-search'><i class="far fa-search"></i></Link>
+                </div>
+            </div>
             <Link to='/listings' className='suprise-me'>Suprise me</Link>
            
         </div>
