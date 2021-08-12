67fcd7c337839aa0c2cf849f2a6746d53f99d282
Jack Liu <Jack.tf.liu@gmail.com>
2021-08-12 19:02:29 -0400

    search bar

--- frontend/components/splash/background_body.jsx
+++ frontend/components/splash/background_body.jsx
@@ -7,20 +7,22 @@ const BackgroundBody = () => {
         <div className='splash-container'>
             <div className='filter-box'>
                 <div className='filter-container'>
-                    <button className='filter-buttons'>
-                    <div>Where are you going?</div></button>
+                    <button className='filter-button-search'>
+                        <span>Where are you going?</span>
+                    </button>
                     <span></span>
                     <input type='date' className='filter-buttons'/>
                     <span></span>
                     <input type='date' className='filter-buttons'/>
                     <span></span>
-                    <button className='filter-buttons'>Guest</button>
+                    <button className='filter-button-guest'>Guest</button>
                     <span></span>
                     <Link to='/listings' className='filter-buttons-search'><i className="fas fa-search"></i></Link>
                 </div>
             </div>
-            <Link to='/listings' className='suprise-me'>Suprise me</Link>
-           
+            <div className='suprise-me-container'> 
+                <Link to='/listings' className='suprise-me'>Suprise me</Link>
+            </div>
         </div>
     )
 }
