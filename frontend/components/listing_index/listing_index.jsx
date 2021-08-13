54bc7be40c279b09af85bb0e1f744775189a0142
Jack Liu <Jack.tf.liu@gmail.com>
2021-08-12 21:35:00 -0400

    index page gif

--- frontend/components/listing_index/listing_index.jsx
+++ frontend/components/listing_index/listing_index.jsx
@@ -16,20 +16,24 @@ class ListingIndex extends React.Component{
         const {listings} = this.props
         return(
             <div className='listing-container'>
-                <div className='listing-left'>
-                    <h1 className='listing-page-header'>Stays in a selected city</h1>
-                    <div className='listing-page-button'>
-                        <button className='curved-button'>Suprise me</button>
-                        <button className='curved-button'>button2</button>
-                        <button className='curved-button'>button3</button>
-                        <button className='curved-button'>button4</button>
-                    </div>
-
-                    <div className='listing-page-words'>More than 240,000 guests have used this website.
-                        On average they rated their stays 4.8 out of 5 stars.
-                    </div>
-                    <div className='listings'>
-                            {listings.map(listing => <ListingIndexItem listing={listing} key={listing.id}/>)} 
+                <div className='listing-left-container'>
+                    <div className='listing-left'>
+                        <h1 className='listing-page-header'>Stays in a selected city</h1>
+                        <div className='listing-page-button'>
+                            <button className='curved-button'>Cancellation</button>
+                            <button className='curved-button'>Cities</button>
+                            <button className='curved-button'>Amentities</button>
+                            <button className='curved-button'>Ratings</button>
+                        </div>
+                        <div className='words-container-index'>
+                            <div className='stays-gif'></div>
+                            <div className='listing-page-words'>More than 240,000 guests have used this website.
+                                On average they rated their stays 4.8 out of 5 stars.
+                            </div>
+                        </div>
+                        <div className='listings'>
+                                {listings.map(listing => <ListingIndexItem listing={listing} key={listing.id}/>)} 
+                        </div>
                     </div>
                 </div>
                 <div className='main-map'>
