2e662d97249f5845ed812be6e3c7ca9c145fa8d9
Jack Liu <Jack.tf.liu@gmail.com>
2021-08-12 21:59:07 -0400

    added index page styling

--- frontend/components/listing_index/listing_index_item.jsx
+++ frontend/components/listing_index/listing_index_item.jsx
@@ -6,28 +6,25 @@ const ListingIndexItem = props => {
 
     return(
         <div className='listing-left-box'>
-
                 <img className='listing-image' src={listing.photos[0]}></img>
-
                 <Link className='listing-items-container' to={`/listings/${listing.id}`}>
                     <h1 className='listing-header'>
                         <div className='listing-description'>{listing.title}</div>
                     </h1>
-
                     <div className='listing-index-am'>
-                        Wifi · Air conditioning · Kitchen · Heating
-                            <br/>
-                            Bedrooms: {listing.bedroom} 
-                            <br/>
-                            Bathrooms: {listing.bathroom}
+                        <p>
+                            Wifi · Air conditioning · Kitchen · Heating
+                        </p>
+                        <br/>
+                        <p>
+                            Bedrooms: {listing.bedroom} · Bathrooms: {listing.bathroom}  
+                        </p>
                     </div>
-
-                    <div>
-                        Rating: {Math.random()}
-                    </div>
-
                     <div className='listing-price'>
-                        ${listing.price} / night
+                        <i className="fa fa-star"> {}</i>
+                        <h1>
+                            ${listing.price}<p>/ night</p>
+                        </h1>
                     </div>
                 </Link>
         </div>
