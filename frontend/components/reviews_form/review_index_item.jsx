7b26c77503fda8cd2ae53daa7a9e52f4a3cbddf8
Jack Liu <Jack.tf.liu@gmail.com>
2021-08-19 17:46:19 -0400

    review bug

--- frontend/components/reviews_form/review_index_item.jsx
+++ frontend/components/reviews_form/review_index_item.jsx
@@ -4,7 +4,7 @@ const ReviewIndexItem = props => {
 
     return (
         <div className='review-container'>
-            <div className='review-container'>
+            <div>
                 {props.currentUser && props.currentUser.id === props.review.user_id ?
                 <div className='x' onClick={() => props.deleteReview(props.review.id)
                 .then(() => {window.location.reload()})}>&times;</div> : '' }
@@ -14,7 +14,6 @@ const ReviewIndexItem = props => {
             </div>{props.review.rating}</div>
             <div>{props.review.review_name}</div>
             <div className='review-comment'>{props.review.body}</div>
-
         </div>
     )
 }
