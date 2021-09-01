7aa4d6f8a1b3c44a4a735d873da523e5b328467f
Jack Liu <Jack.tf.liu@gmail.com>
2021-08-31 21:15:06 -0400

    fixed coord filters

--- frontend/components/listing_index/listing_index_container.js
+++ frontend/components/listing_index/listing_index_container.js
@@ -10,11 +10,11 @@ const mSTP = (state) => ({
     filter: state.ui.filter.location,
     changeMap: {
         "NewYorkCity": {coords: [40.76, -73.98], zoom: 14 }, 
-        "Miami": {coords: [25.7617, 80.1918], zoom: 13}, 
+        "Miami": {coords: [25.830500, -80.180374], zoom: 13}, 
         "Chicago": {coords: [41.8821009, -87.6377712], zoom: 14}, 
-        "LosAngeles": {coords: [34.0450184, -118.3421892], zoom: 11}, 
-        "LasVegas": {coords: [36.1699,115.1398], zoom: 13}, 
-        "AtlanticCity": {coords: [39.3643, 74.4229], zoom: 13}
+        "LosAngeles": {coords: [34.0450184, -118.3421892], zoom: 14}, 
+        "LasVegas": {coords: [36.114704, -115.201462], zoom: 13}, 
+        "AtlanticCity": {coords: [39.362129, -74.443108], zoom: 14}
     }
 })
 
